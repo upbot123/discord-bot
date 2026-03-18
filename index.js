b8b9a293b9068a22e78af1189496583cc4594cae
upbot123 <hienhauu30@gmail.com>
2026-03-18 11:48:31 +0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("BOT STARTING...");
+
 const express = require("express");
 const app = express();
 
@@ -8,23 +10,36 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 
+// web cho Render
 app.get("/", (req, res) => {
   res.send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
-
-client.once("ready", async () => {
-  console.log(`Logged in as ${client.user.tag}`);
-
-  const channel = await client.channels.fetch(process.env.CHANNEL_ID);
-
-  joinVoiceChannel({
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    adapterCreator: channel.guild.voiceAdapterCreator,
-  });
+app.listen(PORT, () => {
+  console.log("WEB OK");
 });
 
-client.login(process.env.TOKEN);
+// bot
+client.on("ready", async () => {
+  console.log("LOGGED IN:", client.user.tag);
+
+  try {
+    const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+    console.log("FOUND CHANNEL");
+
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+
+  } catch (err) {
+    console.log("CHANNEL ERROR:", err.message);
+  }
+});
+
+// login
+client.login(process.env.TOKEN).catch(err => {
+  console.log("TOKEN ERROR:", err.message);
+});
