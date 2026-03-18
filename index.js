dc26bb7895d4d4fc9a4c55421c25f2b57e86608f
upbot123 <hienhauu30@gmail.com>
2026-03-18 11:20:33 +0700

    index.js

--- index.js
+++ index.js
@@ -18,3 +18,35 @@ client.once("ready", async () => {
 });
 
 client.login(process.env.TOKEN); 
+const express = require("express");
+const app = express();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
+client.once("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+});
+
+client.login(process.env.TOKEN);
