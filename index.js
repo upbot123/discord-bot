2cfea767ce510920c397f04b3e157a4128c4e9ea
upbot123 <hienhauu30@gmail.com>
2026-03-18 10:45:48 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+client.once("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+});
+
+client.login(process.env.TOKEN); 
